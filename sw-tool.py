0a706ff63dc2023c3c6a60f10a353c6cca7842b5
zhuliu4 <328260148@qq.com>
2022-10-11 18:55:08 +0800

    Compare voiceprints with process pool

--- sw-tool.py
+++ sw-tool.py
@@ -9,20 +9,12 @@ import requests
 from kafka import KafkaProducer, KafkaConsumer
 import configparser
 import os
-from multiprocessing import Process
+from multiprocessing import Process, cpu_count
 import pandas as pd
-
-"""
-使用python直接运行脚本，可以使用  __file__
-如果打包成exe在windows运行，需要使用  os.path.realpath(sys.argv[0])
-"""
-# project_path = os.path.dirname(__file__)
-project_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.join(project_path, 'config'), 'config.ini'), encoding='utf-8')
-consumer_timeout_ms = int(config['config']['consumer_timeout_ms'])
+from collections import defaultdict
 
 
+# 提供消息体，用于传入提取声纹的kafka topic
 def prepare_message(item) -> (str, str):
     fileName = item.split('/')[-1]
     aid = fileName.split('.')[0]
@@ -68,9 +60,10 @@ def send_to_kfk(text, input_topic, kafka_server):
 
 
 # 向输出队列获取消息(持续获取)，消费队列
-def recv_from_kfk(topic: str, bootstrap_servers: str, auto_offset_reset='latest', file_num=None, file=None):
+def recv_from_kfk(topic: str, bootstrap_servers: str, auto_offset_reset='latest', timeout_ms=10000, file_num=None,
+                  file=None):
     consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers, auto_offset_reset=auto_offset_reset,
-                             consumer_timeout_ms=consumer_timeout_ms)
+                             consumer_timeout_ms=timeout_ms)
     # consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers, auto_offset_reset=auto_offset_reset)
     count = 0
     logger = get_logger()
@@ -80,22 +73,29 @@ def recv_from_kfk(topic: str, bootstrap_servers: str, auto_offset_reset='latest'
         fileinfo = value.get('properties').get('fileInfo')
         fileinfo = json.loads(fileinfo)
         body = json.loads(value.get('body'))
-        is_success = body.get('body')[0].get('tracks')[0].get('state').get('success')  # True:成功，False:失败
+        # is_success = body.get('body')[0].get('tracks')[0].get('state').get('success')  # True:成功，False:失败
+        # is_success = body.get('body')[0].get('tracks')[0].get('items')[0].get('model').get('state').get('success') # True:成功，False:失败
         app = fileinfo.get('app')
+        fileName = fileinfo.get('fileName')
         if app == 'sw-tool' and file_num:
-            sw = body.get('body')[0].get('tracks')[0].get('items')[0].get('model').get(
-                'model') if is_success else 'task_failed'
-            fileName = fileinfo.get('fileName')
+            try:
+                sw = body.get('body')[0].get('tracks')[0].get('items')[0].get('model').get('model')
+                sw = 'task_failed' if sw is None else sw
+            except Exception as e:
+                # raise '文件：%s 的声纹提取失败！标记task_failed' % fileName
+                sw = 'task_failed'
+
             if count == 0:
                 with open(file, 'w', encoding='utf8') as f:
                     f.write(fileName + ' ' + sw)
-                    logger.info('正在提取文件：%s的声纹' % fileName)
+                    logger.info('正在保存文件：%s的声纹' % fileName)
                     count += 1
             elif count <= file_num - 1:
                 with open(file, 'a', encoding='utf8') as f:
                     f.write('\n' + fileName + ' ' + sw)
-                    logger.info('正在提取文件：%s的声纹' % fileName)
+                    logger.info('正在保存文件：%s的声纹' % fileName)
                     count += 1
+
         if count == file_num:  # 数据量与输入的一致则直接退出消费，否则通过 consumer_timeout_ms 等待超时后退出
             logger.info('该批次数据声纹提取结束')
             break
@@ -120,26 +120,57 @@ def get_logger():
     return sw_logger
 
 
+def process_compare(sample, sample_sw, trash_lib, trash_lib_sw, request_url, headers):
+    # def process_compare(_list):
+    body = "[" + '\"' + sample_sw + '\",\"' + trash_lib_sw + '\"' + "]"
+    response = requests.post(url=request_url, headers=headers, data=body)
+    # response = requests.post(url=_list[4], headers=_list[5], data=body)
+    try:
+        # score.append(float(ret))  # 记录得分
+        return sample, json.loads(response.text).get('body')
+    except Exception as e:
+        logger.info(sample + " 与 " + trash_lib + "声纹比对失败，请核查！")
+        raise e
+
+
+def collect_data(_tuple: tuple):
+    global scores
+    scores.append(_tuple[1])
+
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()
+    """
+    使用python直接运行脚本，可以使用  __file__
+    如果打包成exe在windows运行，需要使用  os.path.realpath(sys.argv[0])
+    """
+    # project_path = os.path.dirname(__file__)
+    project_path = os.path.dirname(os.path.realpath(sys.argv[0]))
     start_time = time.time()  # 记录开始时间
-
     """生成日志对象"""
     logger = get_logger()
 
-    """读取配置文件"""
-    # config = configparser.ConfigParser()
-    # config.read(os.path.join(os.path.join(project_path, 'config'), 'config.ini'), encoding='utf-8')
-    kafka_server = config['config']['kafka_server']
-    input_topic = config['config']['input_topic']
-    output_topic = config['config']['output_topic']
-    import_file = os.path.join(project_path, config['config']['import'])
-    output_file = os.path.join(project_path, config['config']['export'])
-    # 读取垃圾音库的配置项，
-    trash_libs_sw = os.path.join(project_path, config['config']['trash_libs_sw'])
-    trash_libs_urls = os.path.join(project_path, config['config']['trash_libs_urls'])
-    # 根据output_file，trash_libs_sw输出的声纹比对结果
-    sw_compare_result = os.path.join(project_path, config['config']['sw_compare_result'])
+    if True:
+        """读取配置文件"""
+        config = configparser.ConfigParser()
+        config.read(os.path.join(os.path.join(project_path, 'config'), 'config.ini'), encoding='utf-8')
+        consumer_timeout_ms = int(config['config']['consumer_timeout_ms'])
+        kafka_server = config['config']['kafka_server']
+        input_topic = config['config']['input_topic']
+        output_topic = config['config']['output_topic']
+        import_file = os.path.join(project_path, config['config']['import'])
+        output_file = os.path.join(project_path, config['config']['export'])
+        # 读取垃圾音库的配置项，
+        trash_libs_sw = os.path.join(project_path, config['config']['trash_libs_sw'])
+        trash_libs_urls = os.path.join(project_path, config['config']['trash_libs_urls'])
+        # 根据output_file，trash_libs_sw输出的声纹比对结果
+        sw_compare_result = os.path.join(project_path, config['config']['sw_compare_result'])
+        sw_compare_api = config['config']['sw_compare']
+        systemWeight = config['config']['systemWeight']
+        try:
+            proc_num = int(config['config']['proc_num'])
+        except Exception:
+            proc_num = cpu_count()
 
     """如果垃圾音声纹库不存在，则调用kfk（引擎）获取"""
     if not os.path.exists(trash_libs_sw):
@@ -148,7 +179,8 @@ if __name__ == '__main__':
         """
         logger.info('开始注册垃圾音声纹库。。。 本次需要注册声纹 %d 条' % num_of_file(trash_libs_urls))
         p1 = Process(target=recv_from_kfk,
-                     args=(output_topic, kafka_server, 'latest', num_of_file(trash_libs_urls), trash_libs_sw))
+                     args=(output_topic, kafka_server, 'latest', consumer_timeout_ms, num_of_file(trash_libs_urls),
+                           trash_libs_sw))
         p1.start()
         logger.info("开始注册垃圾音声纹库")
         sleep(3)  # 睡眠一段时间，给消费程序充足的连接准备时间，避免马上生产消息导致最早的部分数据丢失
@@ -159,83 +191,59 @@ if __name__ == '__main__':
         p1.join()
         logger.info("注册垃圾音声纹库完毕")
 
-    """开始获取样本的声纹数据"""
-    p2 = Process(target=recv_from_kfk,
-                 args=(output_topic, kafka_server, 'latest', num_of_file(import_file), output_file))
-    p2.start()
-    logger.info('开始生成样本的声纹数据')
-    sleep(3)  # 睡眠一段时间，给消费程序充足的连接准备时间，避免马上生产消息导致最早的部分数据丢失
-    with open(import_file, 'r') as f:
-        for item in f.read().splitlines():
-            send_to_kfk(item, input_topic, kafka_server)
-    p2.join()
-    logger.info('样本数据的声纹已提取，请查看输出文件')
+    """如果样本文件声纹不存在，则开始获取样本的声纹数据"""
+    if not os.path.exists(output_file):
+        p2 = Process(target=recv_from_kfk,
+                     args=(
+                         output_topic, kafka_server, 'latest', consumer_timeout_ms, num_of_file(import_file),
+                         output_file))
+        p2.start()
+        logger.info('开始生成样本的声纹数据')
+        sleep(3)  # 睡眠一段时间，给消费程序充足的连接准备时间，避免马上生产消息导致最早的部分数据丢失
+        with open(import_file, 'r') as f:
+            for item in f.read().splitlines():
+                send_to_kfk(item, input_topic, kafka_server)
+        p2.join()
+        logger.info('样本数据的声纹已提取，请查看输出文件')
 
     """开始进行声纹比对"""
+    logger.info("开始进行声纹比对,自动过滤没有声纹的数据,进程数：%d", proc_num)
     # 请求头
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/100.0.4896.75 Safari/537.36',
         'Content-Type': 'application/json'
     }
-    sw_compare_api = config['config']['sw_compare']
-    systemWeight = config['config']['systemWeight']
     url_suffix = ''
     for _ in systemWeight.split():
         url_suffix += 'systemWeight=' + _ if not url_suffix else '&systemWeight=' + _
     request_url = "http://" + sw_compare_api + "/tuling/mlv/v2/vsPlusCheckResource?" + url_suffix
-    lines = 0  # 已写文件的行数
 
     df = pd.DataFrame(columns=['样本文件', '与垃圾音库比对的最高得分'])
-    score = []
-
+    pool = multiprocessing.Pool(proc_num)
+    pool_tasks = []
     with open(output_file, 'r') as f:
         for f_item in f.read().splitlines():  # f_item 包含样本的声纹数据
+            scores = []
             f_item = f_item.split()
             sw1 = f_item[1]
             new_line = [f_item[0]]  # 待新增的数据，首先传入样本文件名
+            if sw1 == "task_failed":
+                continue
+            logger.info("开始获取文件 %s 和 垃圾音库的比对结果，请稍等~" % f_item[0])
             with open(trash_libs_sw, 'r') as g:
                 for g_item in g.read().splitlines():  # g_item 包含垃圾音库声纹数据
                     g_item = g_item.split()
                     sw2 = g_item[1]
-                    body = "[" + '\"' + sw1 + '\",\"' + sw2 + '\"' + "]"
-                    response = requests.post(request_url, headers=headers, data=body)
-                    ret = json.loads(response.text).get('body')
-                    score.append(float(ret))  # 记录得分
-
-            new_line.append(max(score))
+                    if sw2 != "task_failed":
+                        task = pool.apply_async(process_compare, (f_item[0], sw1, g_item[0], sw2, request_url, headers),
+                                                callback=collect_data)
+                        pool_tasks.append(task)
+            [_.wait() for _ in pool_tasks]
+            new_line.append(max(scores))
             df.loc[len(df)] = new_line
-            lines += 1
+    # pool.close()
     logger.info('正在记录声纹比对得分。。。')
     df.to_excel(sw_compare_result, index=False)
     end_time = time.time()  # 记录程序结束时间
     logger.info("本次SW-TOOL程序耗时 %.2f秒" % (end_time - start_time))
-
-"""
-    v1.0.0版本
-    with open(output_file, 'r') as f:
-        for f_item in f.read().splitlines():  # f_item 包含样本的声纹数据
-            f_item = f_item.split()
-            sw1 = f_item[1]
-            txt = ''
-            with open(trash_libs_sw, 'r') as g:
-                for g_item in g.read().splitlines():  # g_item 包含垃圾音库声纹数据
-                    g_item = g_item.split()
-                    sw2 = g_item[1]
-                    body = "[" + '\"' + sw1 + '\",\"' + sw2 + '\"' + "]"
-                    response = requests.post(request_url, headers=headers, data=body)
-                    ret = json.loads(response.text).get('body')
-                    txt += '\t' + g_item[0] + '\t' + str(ret)
-            if lines == 0:
-                with open(sw_compare_result, 'w') as h:  # h写入声纹比对结果
-                    h.write(f_item[0] + txt)
-                    logger.info('正在写入文件：%s的声纹比对结果' % f_item[0])
-                    df = pd.DataFrame(columns=)
-                    df.loc[]
-            else:
-                with open(sw_compare_result, 'a') as h:
-                    h.write('\n' + f_item[0] + txt)
-                    logger.info('正在写入文件：%s的声纹比对结果' % f_item[0])
-            lines += 1
-"""
-
